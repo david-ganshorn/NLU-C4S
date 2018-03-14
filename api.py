e7c08260b551b08d4459372737137f96e536c6d4
d059440 <david.ganshorn@sap.com>
2018-03-14 14:29:55 +0100

    api test

--- api.py
+++ api.py
@@ -1,13 +1,17 @@
 
 
 # Main deploy function
+
+
 def label_review(new_review):
 
     print("Hello World")
+    print(new_review)
+
+    return new_review
 
 
 
 if __name__ == '__main__':
 
     print("Hello WOrld")
-
